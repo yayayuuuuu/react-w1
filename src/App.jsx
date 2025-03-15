32c5f4dd44b1521fcaa85ec4cf0e2cf58a512448
yayayuuuuu <s111219031@stu.ntue.edu.tw>
2025-03-15 22:57:25 +0800

    1

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,7 @@ const HomeSection = () => {
       </p>
       <div className="d-flex justify-content-center gap-3">
         <a href="商品簡介/product.html">
-          <img id="home_product2" src="/image/home_product2.webp" className="zoom-image w-25" style={{ width: 500, height: "auto" }} alt="product 2" />
+          <img id="home_product2" src="/image/home_product2.webp" className="zoom-image w-25" style={{ width: 1000 }} alt="product 2" />
         </a>
         <a href="商品簡介/product.html">
           <img id="home_product3" src="/image/home_product3.webp" className="zoom-image w-25" alt="product 3" />
