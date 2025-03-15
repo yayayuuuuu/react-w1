b12f8191673fd0a3fef003b046cc8a38320c3f30
yayayuuuuu <s111219031@stu.ntue.edu.tw>
2025-03-15 22:49:43 +0800

    a

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ const HomeSection = () => {
         <br />
         不只是一條手鍊，更是一場關於海洋與自己的對話
       </p>
-      <div className="d-flex justify-content-center gap-3" style="width: 200px; height: auto;">
+      <div className="d-flex justify-content-center gap-3" style={{ width: 200, height: "auto" }}>
         <a href="商品簡介/product.html">
           <img id="home_product2" src="/image/home_product2.webp" className="zoom-image w-25" alt="product 2" />
         </a>
